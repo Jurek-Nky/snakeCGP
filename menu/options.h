c98ecb12f7e7093991e2fa1fe5d19ff798683b16
jurek-nky <jurek.nit@gmail.com>
2022-11-26 14:00:54 +0100

    added static options class to save global state of the application

---
+++ menu/options.h
@@ -0,0 +1,19 @@
+#ifndef SNAKECGP_OPTIONS_H
+#define SNAKECGP_OPTIONS_H
+
+
+class Options {
+public:
+    static bool musicEnabled;
+    static int musicLevel;
+    static bool soundEffectsEnabled;
+    static int soundEffectLevel;
+    static bool soundEnabled;
+    static int soundOverallLevel;
+
+    static bool camThirdPersonEnabled;
+    static float camZoom;
+};
+
+
+#endif //SNAKECGP_OPTIONS_H
